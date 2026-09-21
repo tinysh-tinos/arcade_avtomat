e05c95dd3257f1ae4fc400d03cad21f394ea999c
TheTinysh <maksimgordienko599@gmail.com>
2026-09-21 11:00:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import sys
+import subprocess
+
+def install_and_import(package):
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+
+install_and_import('customtkinter')
+
 import os
 import customtkinter as ctk
 
